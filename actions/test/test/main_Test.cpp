79eb6294a75b88f2b8c21a76deb547843eb3218a
Michael Beckh <8502359+mbeckh@users.noreply.github.com>
2020-06-27 11:04:07 +0200

    Better test output

--- actions/test/test/main_Test.cpp
+++ actions/test/test/main_Test.cpp
@@ -5,10 +5,11 @@
 int main(int argc, char** argv) {
 	for (int i = 0; i < argc; ++i) {
 		if (function(i) != 0) {
-			std::cout << "[ERROR] arg " << i;
+			std::cout << "[ERROR] arg " << i << std::endl;
 			return 1;
 		}
-		std::cout << "[OK] arg " << i;
+		std::cout << "[OK] arg " << i << std::endl;
 	}
+	std::cout << "[DONE] Tests completed" << std::endl;
 	return 0;
 }
